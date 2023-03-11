6bd0b85fe575a7883a50943c9bdf2415c3329506
Sing <16409791+singyiu@users.noreply.github.com>
2023-03-11 08:05:08 -0800

    hardhat template

---
+++ hardhat/deploy/00_deploy.js
@@ -0,0 +1,20 @@
+// deploy/00_deploy_my_contract.js
+// https://github.com/wighawag/hardhat-deploy
+
+const hre = require("hardhat");
+
+module.exports = async ({getNamedAccounts, deployments}) => {
+  const {deploy} = deployments;
+  const { deployer } = await getNamedAccounts();
+  
+  const uri = "https://test";
+  const beneficiary = "0x683c5FEb93Dfe9f940fF966a264CBD0b59233cd2";
+  const mintFee = hre.ethers.utils.parseEther("0.0001");
+
+  await deploy('NpNFT', {
+    from: deployer,
+    args: [uri, beneficiary, mintFee],
+    log: true,
+  });
+};
+module.exports.tags = ['NpNFT'];
---
+++ hardhat/hardhat.config.js
@@ -0,0 +1,21 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("hardhat-deploy");
+require("hardhat-deploy-ethers");
+
+const defaultNetwork = "localhost";
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  defaultNetwork,
+  solidity: "0.8.17",
+  networks: {
+    localhost: {
+      url: "http://localhost:8545",
+    },
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0, // here this will by default take the first account as deployer
+    },
+  },  
+};
